b2568dd8b7f1ac05b6ca52f5412e029cd1547005
student <student@Y5-1>
2022-11-16 20:22:00 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    DateTime? seletedDate = calendar.SelectedDate;
+        //    MessageBox.Show(seletedDate.Value.Date.ToShortDateString());
+        //}
+    }
+}
